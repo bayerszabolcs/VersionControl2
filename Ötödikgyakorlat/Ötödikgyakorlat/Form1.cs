c58296852058be79a3a24d0dfc84f6fb4d600d55
Szabolcs Bayer <szabolcs.bayer@stud.uni-corvinus.hu>
2022-10-21 14:46:44 +0200

    5/XmlFeldolgozása

--- Ötödikgyakorlat/Ötödikgyakorlat/Form1.cs
+++ Ötödikgyakorlat/Ötödikgyakorlat/Form1.cs
@@ -21,14 +21,14 @@ namespace Ötödikgyakorlat
             InitializeComponent();
             dataGridView1.DataSource = Rates;
 
-            MnbDataAdd();
-            XMLProcess();
+            
+            XMLProcess(MnbDataAdd());
 
         }
 
         
 
-        private static void MnbDataAdd()
+        public string MnbDataAdd()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
 
@@ -41,10 +41,31 @@ namespace Ötödikgyakorlat
 
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
+
+            return result;
         }
-        private void XMLProcess()
+        public void XMLProcess(string data)
         {
-            
+            var xml = new XmlDocument();
+            xml.LoadXml(data);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+
+            }
+
         }
 
     }
