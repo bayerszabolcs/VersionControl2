598c0b274ab90ff7e9c684040529a92c33027ec8
Szabolcs Bayer <szabolcs.bayer@stud.uni-corvinus.hu>
2022-11-14 22:20:23 +0100

    create ballfactory

---
+++ 5_het/5_het/Entities/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_het.Entities
+{
+    class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
