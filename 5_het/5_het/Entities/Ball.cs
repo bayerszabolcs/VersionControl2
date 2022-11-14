6e0b933bb2b5c128acc982c0394ba36cc1cba3fe
Szabolcs Bayer <szabolcs.bayer@stud.uni-corvinus.hu>
2022-11-14 22:57:49 +0100

    creating futószalag

--- 5_het/5_het/Entities/Ball.cs
+++ 5_het/5_het/Entities/Ball.cs
@@ -8,7 +8,7 @@ using System.Windows.Forms;
 
 namespace _5_het.Entities
 {
-    class Ball : Label
+    public class Ball : Label
     {
         public Ball()
         {
--- 5_het/5_het/Entities/BallFactory.cs
+++ 5_het/5_het/Entities/BallFactory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace _5_het.Entities
 {
-    class BallFactory
+    public class BallFactory
     {
         public Ball CreateNew()
         {
