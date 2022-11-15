2cb801840fd6168ed976b877d7776523ba9392ba
Szabolcs Bayer <szabolcs.bayer@stud.uni-corvinus.hu>
2022-11-15 16:03:17 +0100

    Factory Abstraction

--- 5_het/5_het/Entities/Car.cs
+++ 5_het/5_het/Entities/Car.cs
@@ -13,7 +13,7 @@ namespace _5_het.Entities
         protected override void DrawImage(Graphics g)
         {
             Image imageFile = Image.FromFile("Images/car.png");
-            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height)); 
         }
     }
 }
