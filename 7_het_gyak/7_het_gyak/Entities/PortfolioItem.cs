cdca952b2ddcae2b8a4cb44436cdb9c34896787b
Szabolcs Bayer <szabolcs.bayer@stud.uni-corvinus.hu>
2022-11-16 22:13:32 +0100

    Orm Object

---
+++ 7_het_gyak/7_het_gyak/Entities/PortfolioItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_het_gyak.Entities
+{
+    class PortfolioItem
+    {
+        public string Index { get; set; }
+        public decimal Volume { get; set; }
+    }
+}
