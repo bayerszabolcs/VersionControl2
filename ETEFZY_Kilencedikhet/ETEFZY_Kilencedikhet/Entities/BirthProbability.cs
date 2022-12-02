bdef94dbc8dca56c1809bad6869d2724adf3ef23
Szabolcs Bayer <szabolcs.bayer@stud.uni-corvinus.hu>
2022-12-02 14:17:47 +0100

    Person BirthProbability DeathProbability classes

---
+++ ETEFZY_Kilencedikhet/ETEFZY_Kilencedikhet/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETEFZY_Kilencedikhet.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+        public int nbrOfChildren { get; set; }
+        public double BirthProb { get; set; }
+    }
+}
